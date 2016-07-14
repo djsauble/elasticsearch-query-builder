a1798dce5fafc7ad601ffe878e5609548d053ed8
Daniel Sauble <djsauble@gmail.com>
2016-07-14 11:37:49 -0700

    Initial commit of query builder skeleton

---
+++ README.md
@@ -0,0 +1,19 @@
+# Query builder prototype for Elasticsearch
+
+## Before you start
+
+* Ensure that an elasticsearch instance is available at `http://localhost:9200`
+* Install dependencies with `npm install`
+
+## Develop
+
+* Install node (>= 4.4.7)
+* Install browsify `npm install browsify -g`
+* Install watchify `npm install watchify -g`
+* Build changes automatically `watchify app/main.js -o public/bundle.js`
+* Run the server `node app.js`
+
+## Run
+
+* `node app.js`
+* View in browser at `http://localhost:3000`
---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const finalhandler = require('finalhandler');
+const serveStatic = require('serve-static');
+
+const serve = serveStatic('./public');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    // Display the index page
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<!DOCTYPE html>\n');
+    res.write('<html>\n');
+    res.write('<head>\n');
+    res.write('<script src="bundle.js"></script>\n');
+    res.write('</head>\n');
+    res.write('<body>\n');
+    res.write('Home page');
+    res.write('</body>\n');
+    res.write('</html>\n');
+    res.end();
+  }
+  else {
+    // Fetch the requested file
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
